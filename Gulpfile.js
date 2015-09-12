bf9da4ee9c0048f8d15252e623ad131b442f3aeb
Pawel Kondraciuk <kondraciukpawel@gmail.com>
2015-09-12 09:58:13 +0200

    Add sass watch

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,41 @@
-var gulp;
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var coffee = require('gulp-coffee');
+var concat = require('gulp-concat');
+var livereload = require('gulp-livereload');
+var plumber = require('gulp-plumber');
+var rename = require('gulp-rename');
+var sass = require('gulp-ruby-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+var util = require('gulp-util');
 
-gulp = require('gulp');
+var bowerDir = './vendor/assets/components';
+var sassDir = './app/assets/stylesheets';
 
-gulp.task('task', function() {
+var config = {
+  sass: {
+    src: [sassDir + '/application.sass'],
+    watchSrc: [sassDir + '/**/*.sass'],
+    options: {
+      sourcemap: true,
+      lineNumbers: true,
+      loadPath: [
+        bowerDir + '/bootstrap-sass/assets/stylesheets',
+        bowerDir + '/font-awesome/scss'
+      ]
+    }
+  }
+}
 
+gulp.task('sass', function() {
+  return sass(config.sass.src, config.sass.options)
+    .pipe(autoprefixer())
+    .pipe(rename('application.css'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./public/assets/'))
+});
+
+gulp.task('watch-sass', function() {
+  gulp.watch(config.sass.watchSrc, ['sass']);
 });
